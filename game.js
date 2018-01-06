719b00c1098bf205e5e79ccb36a598f02e00e05a
Aninda <aksumit95@gmail.com>
2018-01-06 17:49:31 +0100

    chat box

--- game.js
+++ game.js
@@ -1,49 +1,76 @@
 var game;
-//adding background color as hex value
+var bgColors = [0xF16745, 0xFFC65D, 0x7BC8A4, 0x4CC3D9, 0x93648D, 0x7c786a, 0x588c73, 0x8c4646, 0x2a5b84, 0x73503c];
+var tunnelWidth = 256;
+var shipHorizontalSpeed = 100;
+var shipMoveDelay = 0;
+var shipVerticalSpeed = 15000; 
+var swipeDistance = 10;
+var barrierSpeed = 280;
+var barrierGap = 120;
+var shipInvisibilityTime = 1000;
 
-var bgColors = [0xF16745, 0xFFC65D, 0x7BC8A4, 0x4CC3D9, 0x93648D, 0x7c786a,
- 0x588c73, 0x8c4646, 0x2a5b84, 0x73503c];
- var tunnelWidth = 256;
+var users = 2;
+var String = ['Online Chat Room', 'Available Users: '];
+var userString = ['sumit: ', 'sajib: '];
+// var userString = localStorage.getItem("auth_user");
+var content_sumit = [
+    " ",
+    "kire koi tui??",
+    "khida lagse amar, kisu ase",
+    " ",
+    "Kon shala ei game banaise",
+    " ",
+    "game e kono level nai",
+    "bal game e ball er matha kharap",
+    "    "
+];
+var content_sajib = [
+    "  ",
+    "Manzke kono mark e dibo na",
+    "duniya te ar kono game chilo na",
+    " ",
+    "na khelle ghuma ga ...",
+    "    ",
+    "ami khelum na baler game",
+    "jaiga bye",
+    "   "
+];
 
- window.onload = function() {	
-	game = new Phaser.Game(640, 960, Phaser.AUTO, "");
-	//Boot state: in the boot state we will make all adjustment to the game to be resized accordingly to browser 
-	//resolution and aspect ratio
-				// name given(key) , function (state)
-     game.state.add("Boot", boot);
-	 
-	//we will use this state to preload all assets we will use in the game.
-	//It's the classic “loading” screen you see in most games 
-     game.state.add("Preload", preload);
-	
-	//the title screen, showing your game name and a play button.
-     game.state.add("TitleScreen", titleScreen);
-	
-	//The game itself
-     game.state.add("PlayGame", playGame);
-	
-	//The game over screen also features a “play again” button to let players restart the game
-     game.state.add("GameOverScreen", gameOverScreen);
-	 
-     game.state.start("Boot"); //starting the first state
+var remotetext1;
+var remotetext2;
+var index = 0;
+var condex = 0;
+var line_sumit = '';
+var line_sajib = '';
+
+window.onload = function() {	
+    game = new Phaser.Game(640, 960, Phaser.AUTO, "");
+                // ("Key", state)
+    game.state.add("Boot", boot);
+    game.state.add("Preload", preload); 
+    game.state.add("TitleScreen", titleScreen);
+    game.state.add("PlayGame", playGame);
+    game.state.add("GameOverScreen", gameOverScreen);
+    
+    game.state.start("Boot");//to start all the state
 }
 
+//This will make all the adjustment to the game according to browser-
+//resolution and aspectratio
 var boot = function(game){};
+
 boot.prototype = {
-	
-  	preload: function(){
-          this.game.load.image("loading","assets/sprites/loading.png"); //will load or show loading progress 
+  	preload: function(){          this.game.load.image("loading","assets/sprites/loading.png"); 
 	},
   	create: function(){
 		game.scale.pageAlignHorizontally = true;
 		game.scale.pageAlignVertically = true;
-		// will set scaling method
         game.scale.scaleMode = Phaser.ScaleManager.SHOW_ALL;
-		// this will pass the state to the next state
 		this.game.state.start("Preload");
-	}      
+	}
 }
 
+//To load all our assets 
 var preload = function(game){};
 preload.prototype = {
 	preload: function(){ 
@@ -52,44 +79,34 @@ preload.prototype = {
           game.load.setPreloadSprite(loadingBar);
           game.load.image("title", "assets/sprites/title.png");
           game.load.image("playbutton", "assets/sprites/playbutton.png");
-		  game.load.image("backsplash", "assets/sprites/backsplash.png");
-		  game.load.image("tunnelbg", "assets/sprites/tunnelbg.png");
+          game.load.image("backsplash", "assets/sprites/backsplash.png");
+          game.load.image("tunnelbg", "assets/sprites/tunnelbg.png");
           game.load.image("wall", "assets/sprites/wall.png");
-		  game.load.image("ship", "assets/sprites/ship.png");
+          game.load.image("ship", "assets/sprites/ship.png");
+          game.load.image("smoke", "assets/sprites/smoke.png");
+          game.load.image("barrier", "assets/sprites/barrier.png");
 	},
   	create: function(){
 		this.game.state.start("TitleScreen");
 	}
 }
 
+//For showing game name and play button
 var titleScreen = function(game){};
+
 titleScreen.prototype = {  
-     create: function(){
-		var titleBG = game.add.tileSprite(0,0,game.width,game.height,
-		"backsplash");
-		titleBG.tint = bgColors[game.rnd.between(0, bgColors.length -1)];
-		//the next line will change background color according to-
-		//defined hex value bgColor[game.rnd.between(0, bgColor.length - 1 )]
-         game.stage.backgroundColor = bgColors[game.rnd.between(0, bgColors.length - 1)];
+     create: function(){  
+         //Background
+         var titleBG = game.add.tileSprite(0, 0, game.width, game.height, "backsplash");
+          titleBG.tint = bgColors[game.rnd.between(0, bgColors.length - 1)];
          
-		 //adding the title or game image
-		 var title = game.add.image(game.width / 2, 210, "title");
-         title.anchor.set(0.5);
-		 //using tween both for title-name and playButton
-		 var tween = game.add.tween(title).to({
-               width: 420,
-               height:420
-          }, 4000, "Linear", true, 0, -1); 
-          tween.yoyo(true);
-		  
-		  //could also change the background color
-		  //title.tint = bgColors[game.rnd.between(0, bgColors.length -1)];
-		 
-		 //adding the play game button 
-         var playButton = game.add.button(game.width / 2, game.height - 150, "playbutton", this.startGame);
-         playButton.anchor.set(0.5);
-		 var tween = game.add.tween(playButton).to({
-               width: 220,
+         //Start button
+          var title = game.add.image(game.width / 2, 210, "title");
+          title.anchor.set(0.5);
+          var playButton = game.add.button(game.width / 2, game.height - 150, "playbutton", this.startGame);
+          playButton.anchor.set(0.5);
+          var tween = game.add.tween(playButton).to({
+               width: 220, //properties, duration, ease, autostart, delay,repeat
                height:220
           }, 1500, "Linear", true, 0, -1); 
           tween.yoyo(true);
@@ -99,32 +116,265 @@ titleScreen.prototype = {
      }
 }
 
+// For playing game -add player, block, player movment
 var playGame = function(game){};
-playGame.prototype = {
-		create: function(){
-			var tintColor = bgColors[game.rnd.between(0,bgColors.length-1)];
-			var tunnelBG = game.add.tileSprite(0,0,game.width,game.height,"tunnelbg");
-			tunnelBG.tint = tintColor;
-			// it is set as left wall of the game world according to game width
-			var leftWallBG = game.add.tileSprite(- tunnelWidth / 2, 0, game.width / 2, game.height, "wall");
-			leftWallBG.tint = tintColor;
-			// it is set as right wall by 
-			var rightWallBG = game.add.tileSprite((game.width + tunnelWidth) / 2, 0, game.width / 2, game.height, "wall");
-			rightWallBG.tint = tintColor;
-			rightWallBG.tileScale.x = -1;
-			
-			// which is game game.width = 640 tunnelWidth = 256 shipPositions = [224,160]
-			this.shipPositions = [(game.width - tunnelWidth) / 2 + 32, (game.width - tunnelWidth) / 2 - 32];
-			// this will load the ship on the left side
-			this.ship = game.add.sprite(this.shipPositions[0], 860, "ship");
-			// to keep track of the side
-			this.ship.side =0;
-			this.ship.anchor.set(0.5);
-			// physics.enable(object, system) creates a default physics body on object using system physics system. 
-			this.game.physics.enable(this.ship, Phaser.Physics.ARCADE);
-		}
+playGame.prototype = {  
+     create: function(){
+          tintColor = bgColors[game.rnd.between(0, bgColors.length - 1)]
+          var tunnelBG = game.add.tileSprite(0, 0, game.width, game.height, "tunnelbg");
+          tunnelBG.tint = tintColor;
+         
+         //leftwall placement
+          var leftWallBG = game.add.tileSprite(- tunnelWidth / 2, 0, game.width / 2, game.height, "wall");
+          leftWallBG.tint = tintColor;
+         
+         //rightwall placement
+          var rightWallBG = game.add.tileSprite((game.width + tunnelWidth) / 2, 0, game.width / 2, game.height, "wall");
+          rightWallBG.tint = tintColor;
+          rightWallBG.tileScale.x = -1;
+         
+          this.barrierGroup = game.add.group(); 
+          this.addBarrier(this.barrierGroup, tintColor);
+          
+          // Virtual Chat Room
+
+          
+         
+          var Text1 = game.add.text(5, 5, String[0], { font: '25px Courier', fill: '#ff0000'});
+          var Text2 = game.add.text(5, 25, String[1] + users, { font: '20px Courier', fill: '#FFC65D'});
+          var userText1 = game.add.text(5, 45, userString[0], { font: '20px Courier', fill: '#00ffff'});
+          remotetext1 = game.add.text(5, 65, '', { font: "15pt Courier", fill: "#00ffff"});
+          var userText2 = game.add.text(5, 85, userString[1] , { font: '20px Courier', fill: '#00ff11'});         
+          nextLine_sumit();
+          
+          // Test on Virtual Chat
+          remotetext2 = game.add.text(5, 105, '', { font: "15pt Courier", fill: "#00ff11"});
+          //nextLine_sajib();
+         
+         //adding ship/player
+          this.shipPositions = [(game.width - tunnelWidth) / 2 + 32, (game.width + tunnelWidth) / 2 - 32];
+          this.ship = game.add.sprite(this.shipPositions[0], 860, "ship");
+         
+         //start from zero as initially is placed on the left side
+          this.ship.side = 0;
+         
+          this.ship.destroyed = false;
+         
+         //
+          this.ship.canMove = true;
+         
+         //
+          this.ship.canSwipe = false;
+         
+          this.ship.anchor.set(0.5);
+          game.physics.enable(this.ship, Phaser.Physics.ARCADE);
+         
+          game.input.onDown.add(this.moveShip, this);
+         
+          game.input.onUp.add(function(){
+               this.ship.canSwipe = false;
+          }, this);
+         
+          this.smokeEmitter = game.add.emitter(this.ship.x, this.ship.y + 10, 20);
+          this.smokeEmitter.makeParticles("smoke");
+         
+         //horizontal and vertical speed of each particle
+          this.smokeEmitter.setXSpeed(-15, 15);
+          this.smokeEmitter.setYSpeed(50, 150);
+         
+         //Changing the transparency
+          this.smokeEmitter.setAlpha(0.5, 1);
+          this.smokeEmitter.start(false, 1000, 40);
+         
+         //Adding rising effect
+          this.verticalTween = game.add.tween(this.ship).to({
+               y: 0
+          }, shipVerticalSpeed, Phaser.Easing.Linear.None, true);
+     },
+     moveShip: function(){
+          this.ship.canSwipe = true;
+         //left and right movement of ship
+          if(this.ship.canMove && !this.ship.destroyed){
+              
+               this.ship.canMove = false;
+               this.ship.side = 1 - this.ship.side;
+               var horizontalTween = game.add.tween(this.ship).to({ 
+                    x: this.shipPositions[this.ship.side]
+               }, shipHorizontalSpeed, Phaser.Easing.Linear.None, true);
+              
+              //ship movement delay
+               horizontalTween.onComplete.add(function(){
+                    game.time.events.add(shipMoveDelay, function(){
+                         this.ship.canMove = true;
+                    }, this);
+               }, this);
+              
+              
+              //Adding effect for ship movement
+               var ghostShip = game.add.sprite(this.ship.x, this.ship.y, "ship");
+               ghostShip.alpha = 0.5;
+               ghostShip.anchor.set(0.5);
+               var ghostTween = game.add.tween(ghostShip).to({
+                    alpha: 0
+               }, 350, Phaser.Easing.Linear.None, true);
+               ghostTween.onComplete.add(function(){
+                    ghostShip.destroy();
+               });
+          }          
+     },
+     update: function(){
+         
+          this.smokeEmitter.x = this.ship.x;
+          this.smokeEmitter.y = this.ship.y;
+         
+         //adding swipe functionalities
+          if(this.ship.canSwipe){
+             if(Phaser.Point.distance(game.input.activePointer.positionDown, game.input.activePointer.position) > swipeDistance){
+                    this.restartShip();          
+               }   
+          }
+         
+          if(!this.ship.destroyed && this.ship.alpha == 1){
+               game.physics.arcade.collide(this.ship, this.barrierGroup, null, function(s, b){
+                    this.ship.destroyed = true
+                    this.smokeEmitter.destroy();
+                    var destroyTween = game.add.tween(this.ship).to({
+                         x: this.ship.x + game.rnd.between(-100, 100),
+                         y: this.ship.y - 100,
+                         rotation: 10
+                    }, 1000, Phaser.Easing.Linear.None, true);
+                    destroyTween.onComplete.add(function(){
+                         var explosionEmitter = game.add.emitter(this.ship.x, this.ship.y, 200);
+                         explosionEmitter.makeParticles("smoke");
+                         explosionEmitter.setAlpha(0.5, 1);
+                         explosionEmitter.minParticleScale = 0.5;
+                         explosionEmitter.maxParticleScale = 2;
+                         explosionEmitter.start(true, 2000, null, 200);
+                         this.ship.destroy();
+                         game.time.events.add(Phaser.Timer.SECOND * 2, function(){
+                              game.state.start("GameOverScreen");
+                         });
+                    }, this);
+               }, this)
+          }
+     },
+     restartShip: function(){
+          if(!this.ship.destroyed && this.ship.alpha == 1){
+               this.ship.canSwipe = false;
+               this.verticalTween.stop();
+              
+               this.ship.alpha = 0.5;
+              //stoping the vertical tween and reposition after swipe
+               this.verticalTween = game.add.tween(this.ship).to({
+                    y: 860  
+               }, 100, Phaser.Easing.Linear.None, true);
+              
+              //after repositioning again start the vertical tween
+               this.verticalTween.onComplete.add(function(){
+                    this.verticalTween = game.add.tween(this.ship).to({
+                         y: 0
+                    }, shipVerticalSpeed, Phaser.Easing.Linear.None, true);
+                   //adding Invisibility to the ship
+                    var alphaTween = game.add.tween(this.ship).to({
+                         alpha: 1     
+                    }, shipInvisibilityTime, Phaser.Easing.Bounce.In, true);        
+               }, this) 
+          }
+     },
+     addBarrier: function(group, tintColor){
+          var barrier = new Barrier(game, barrierSpeed, tintColor);
+          game.add.existing(barrier);
+          group.add(barrier); 
+     }   
+}
+
+
+function updateLine_sumit() {
+
+    if (line_sumit.length < content_sumit[index].length)
+    {
+        line_sumit = content_sumit[index].substr(0, line_sumit.length + 1);
+        // text.text = line;
+        remotetext1.setText(line_sumit);
+    }
+    else
+    {
+        //  Wait 2 seconds then start a new line
+        game.time.events.add(Phaser.Timer.SECOND * 5, nextLine_sumit, this);
+    }
+
+}
+
+function nextLine_sumit() {
+
+    index++;
+
+    if (index < content_sumit.length)
+    {
+        line_sumit = '';
+        game.time.events.repeat(80, content_sumit[index].length + 1, updateLine_sumit, this);
+    }
+
+}
+
+function updateLine_sojib() {
+
+    if (line_sojib.length < content_sojib[condex].length)
+    {
+        line_sojib = content_sojib[condex].substr(0, line_sojib.length + 1);
+        // text.text = line;
+        remotetext2.setText(line_sojib);
+    }
+    else
+    {
+        //  Wait 2 seconds then start a new line
+        game.time.events.add(Phaser.Timer.SECOND * 5, nextLine_sojib, this);
+    }
+
+}
+
+function nextLine_sojib() {
+
+    condex++;
+
+    if (condex < content_sojib.length)
+    {
+        line_sojib = '';
+        game.time.events.repeat(80, content_sojib[condex].length + 1, updateLine_sojib, this);
+    }
+
 }
 
 var gameOverScreen = function(game){};
-gameOverScreen.prototype = {    
+gameOverScreen.prototype = {
+     create:function(){
+          console.log("game over");
+     }    
+}
+
+Barrier = function (game, speed, tintColor) {
+     var positions = [(game.width - tunnelWidth) / 2, (game.width + tunnelWidth) / 2];
+     var position = game.rnd.between(0, 1);
+	Phaser.Sprite.call(this, game, positions[position], -100, "barrier");
+     var cropRect = new Phaser.Rectangle(0, 0, tunnelWidth / 2, 24);
+     this.crop(cropRect);
+	game.physics.enable(this, Phaser.Physics.ARCADE);
+     this.anchor.set(position, 0.5);
+     this.tint = tintColor;     
+     this.body.immovable = true;
+     this.body.velocity.y = speed;
+     this.placeBarrier = true;
+};
+
+Barrier.prototype = Object.create(Phaser.Sprite.prototype);
+Barrier.prototype.constructor = Barrier;
+
+Barrier.prototype.update = function(){
+     if(this.placeBarrier && this.y > barrierGap){
+          this.placeBarrier = false;
+          playGame.prototype.addBarrier(this.parent, this.tint);
+     }   
+     if(this.y > game.height){
+          this.destroy();
+     }
 }
---
+++ js/login.js
@@ -0,0 +1,27 @@
+var attempt = 3; //Variable to count number of attempts
+
+
+//Below function Executes on click of login button
+function validate(){
+	var username = document.getElementById("username").value;
+	var password = document.getElementById("password").value;
+
+	if ( username == "Zigzag" && password == "Zigzag#123"){
+        
+		alert ("Login successfully");
+		window.location = "index.html"; //redirecting to other page
+		return false;
+	}
+	else{
+		attempt --;//Decrementing by one
+		alert("You have left "+attempt+" attempt;");
+		
+		//Disabling fields after 3 attempts
+		if( attempt == 0){
+			document.getElementById("username").disabled = true;
+			document.getElementById("password").disabled = true;
+			document.getElementById("Sign in").disabled = true;
+			return false;
+		}
+	}
+}
